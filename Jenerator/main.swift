58e439cc8f3febfcc3898784653d7d5f232af6ba
Romain Menke <romainmenke@gmail.com>
2016-06-01 18:11:11 +0200

    Initial Commit

---
+++ Jenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Jenerator
+//
+//  Created by XcodeBuildServer on 01/06/16.
+//  Copyright © 2016 menke-dev. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
