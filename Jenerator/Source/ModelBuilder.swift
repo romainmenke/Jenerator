740035d05d683cfbab0a297a552868622de61fc2
romainmenke <romainmenke@gmail.com>
2016-06-05 18:38:02 +0200

    (add) lots of documentation

--- Jenerator/Source/ModelBuilder.swift
+++ Jenerator/Source/ModelBuilder.swift
@@ -8,21 +8,51 @@
 
 import Foundation
 
+/**
+ *  JSON Analyser
+ */
 public struct ModelBuilder {
     
+    /// Types Found in the JSON data
     var types : [JSONCustomType] = []
+    
+    /// Type Aliasses for Types with the same fields
     var typeAliasses : [JSONTypeAlias] = []
+    
+    /// Root Object for the query and top level objects
     var root : String
+    
+    /// top level object is a dictionary, set to false if it is an array
     var dictionaryAtRoot : Bool = true
+    
+    /// Source Url as String
     var source : String?
+    
+    /// Class Prefix for all Types
     var classPrefix : String
 
+    /**
+     Initialiser
+     
+     - parameter root:        Name for Root Object
+     - parameter classPrefix: Class Prefix for All Types
+     - parameter source:      Source Url as String
+     
+     - returns: a ModelBuilder
+     */
     public init(rootName root:String, classPrefix: String, source:String? = nil) {
         self.root = root
         self.classPrefix = classPrefix
         self.source = source
     }
     
+    /**
+     Array of AnyObject contains Dictionary
+     
+     - parameter array: an Array
+     
+     - returns: true if Array.Element is of Type Dictionary
+     */
     func arrayContainsObjects(array : [AnyObject]) -> Bool {
         var current = array
         while let first = current.first as? [AnyObject] {
@@ -36,6 +66,14 @@ public struct ModelBuilder {
         }
     }
     
+    /**
+     Helper for multi-dimensional Arrays
+     
+     - parameter name:  name of the Type
+     - parameter array: Array
+     
+     - returns: a JSONDataType and data of the first element that is not an Array
+     */
     func buildMultiDimentionalArrayOfObject(name:String, array:[AnyObject]) -> (dataType:JSONDataType,content:[String:AnyObject]?) {
         var type = JSONDataType.JSONArray(type: JSONDataType.JSONNull)
         var current = array
@@ -52,6 +90,13 @@ public struct ModelBuilder {
         return (type,nil)
     }
     
+    /**
+     Start of Analysis
+     
+     - parameter data: Data returned from an API call or read from a .json file
+     
+     - returns: A ModelBuilder containing the constructed Model
+     */
     public func buildModel(data:AnyObject) -> ModelBuilder {
         if let data = data as? [String:AnyObject] {
             return self.startWithDictionary(data)
@@ -61,6 +106,13 @@ public struct ModelBuilder {
         return self
     }
     
+    /**
+     Top level Object is an Array
+     
+     - parameter data: Data from JSON
+     
+     - returns: A ModelBuilder containing the constructed Model
+     */
     private func startWithArray(data:[AnyObject]) -> ModelBuilder {
         var copy = self
         copy.dictionaryAtRoot = false
@@ -87,12 +139,28 @@ public struct ModelBuilder {
         return copy
     }
     
+    /**
+     Top level Object is a Dictionary
+     
+     - parameter data: Data from JSON
+     
+     - returns: A ModelBuilder containing the constructed Model
+     */
     private func startWithDictionary(data:[String:AnyObject]) -> ModelBuilder {
         var copy = self
         copy.types = copy.buildTypes(copy.types, name: copy.root, fields: data)
         return copy
     }
     
+    /**
+     Construct Types
+     
+     - parameter existingTypes: Current found Types
+     - parameter name:          Field name
+     - parameter fields:        Data for the field
+     
+     - returns: An Array of Types
+     */
     private func buildTypes(existingTypes:[JSONCustomType], name:String, fields:[String:AnyObject]) -> [JSONCustomType] {
         
         var currentTypes = existingTypes
@@ -179,6 +247,11 @@ public struct ModelBuilder {
         
     }
     
+    /**
+     Strip duplicate Types from the model
+     
+     - returns: A ModelBuilder without duplicate Types but with Type Aliasses
+     */
     public func findAliasses() -> ModelBuilder {
         var copy = self
         
--- Jenerator/Source/SourceToBuilder.swift
+++ Jenerator/Source/SourceToBuilder.swift
@@ -10,6 +10,14 @@ import Foundation
 
 extension ModelBuilder {
     
+    /**
+     Constructer for a remote JSON source
+     
+     - parameter url:         Url that will return the JSON
+     - parameter classPrefix: Class Prefix for the Types
+     
+     - returns: an initialised ModelBuilder if the url returned valid JSON data
+     */
     public static func fromSource(url:NSURL, classPrefix:String) -> ModelBuilder? {
         
         guard let data = NSData(contentsOfURL: url) else {
@@ -22,6 +30,14 @@ extension ModelBuilder {
         return nil
     }
     
+    /**
+     Constructer for a local JSON source
+     
+     - parameter path:        Path to a .json file as a string
+     - parameter classPrefix: Class Prefix for the Types
+     
+     - returns: an initialised ModelBuilder if the file contained valid JSON data
+     */
     public static func fromFile(path:String, classPrefix:String) -> ModelBuilder? {
         
         guard let data = NSData(contentsOfFile: path) else {
